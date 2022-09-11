62c7b61e94fb3c22f7fad1c9b25383d6b64040f5
Jeremy Wagerik <jeremy.wagerik@gmail.com>
2022-09-11 15:08:47 -0400

    Deliverable 2 complete

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,17 +1,21 @@
 library(dplyr)
 library(jsonlite)
 
+##Deliverable 1
+
 MechaCar_mpg <- read.csv(file = 'MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)#import MechaCar_mpg data file
 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg) #generate multiple linear regression model
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg)) #summary statistics
 
+##Deliverable 2
 
+Suspension_data <- read.csv(file = 'Suspension_Coil.csv', check.names = F, stringsAsFactors = F)#import suspension coil data file
 
+total_summary <- Suspension_data %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
 
-
-
+lot_summary <- Suspension_data %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
 
 
 
